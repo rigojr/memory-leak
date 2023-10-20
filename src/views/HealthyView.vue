e62f3cca17c0c2eb24fe500aeefb78cb461c7702
José Salas <jose.salas@smartmatic.com>
2023-10-20 18:11:32 -0500

    feat: add healthy and unhealthy use still wip

---
+++ src/views/HealthyView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+const add = ref([]);
+
+function onAddClick() {
+  const bigArrayBuffer = new ArrayBuffer(2 ** 20 - 1);
+
+  add.value.push(bigArrayBuffer);
+}
+
+function onCleanClick() {
+  add.value = [];
+}
+</script>
+
+<template>
+  <main>
+      <button @click="onAddClick">ADD</button>
+      <button @click="onCleanClick">CLEAR</button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center
+}
+
+button {
+  width: 100%;
+  height: 36px;
+  margin: 24px;
+}
+</style>
---
+++ src/views/UnhealthyView.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+function onAddClick() {
+  window.addEventListener('click', () => {
+    const bigArrayBuffer = new ArrayBuffer(2 ** 20 - 1);
+
+    if (!window.dirtyThing) { window.dirtyThing = [] }
+
+    window.dirtyThing.push(bigArrayBuffer);
+  })
+}
+
+</script>
+
+<template>
+  <main>
+    <button @click="onAddClick">ADD</button>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center
+}
+
+button {
+  width: 100%;
+  height: 36px;
+  margin: 24px;
+}
+</style>
