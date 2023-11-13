8af74c3e7fb9c22a64889cda28d6d818a4761233
José Salas <jose.salas@smartmatic.com>
2023-11-13 11:52:24 -0500

    refactor: improve healthy look of memory leak

--- src/views/HealthyView.vue
+++ src/views/HealthyView.vue
@@ -1,22 +1,36 @@
 <script setup>
 import { ref } from 'vue';
 
-const add = ref([]);
+const localThing = new Map();
+const clickCount = ref(0);
+const listenerCount = ref(0);
 
-function onAddClick() {
+function crazyThing() {
   const bigArrayBuffer = new ArrayBuffer(2 ** 20 - 1);
 
-  add.value.push(bigArrayBuffer);
+  localThing.set(`${listenerCount.value}`, bigArrayBuffer);
+
+  listenerCount.value = listenerCount.value + 1;
+}
+
+function onAddClick() {
+  window.addEventListener('click', crazyThing);
+
+  clickCount.value = 1 + clickCount.value;
 }
 
 function onCleanClick() {
-  add.value = [];
+  window.removeEventListener('click', crazyThing);
+  clickCount.value = 0;
+  listenerCount.value = 0;
 }
 </script>
 
 <template>
   <main>
       <button @click="onAddClick">ADD</button>
+      <p>Click counts {{ clickCount }}</p>
+      <p>Listeners counts {{ listenerCount }}</p>
       <button @click="onCleanClick">CLEAR</button>
   </main>
 </template>
@@ -33,4 +47,4 @@ button {
   height: 36px;
   margin: 24px;
 }
-</style>
+</style> 
